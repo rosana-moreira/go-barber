ee758de8f3d78e54051ec65b58061214b58d549a
Rosana Moreira <rooh.moreiraa@gmail.com>
2021-06-16 19:43:56 -0300

    Pagina de login prota

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const App: React.FC =()=>(
-  <h1>Hello World</h1>
-)
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SingnIn/index'
+import SingIn from './pages/SingnIn/index';
 
+const App: React.FC = () => {
+  return (
+    <>
+      <SingIn/>
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
