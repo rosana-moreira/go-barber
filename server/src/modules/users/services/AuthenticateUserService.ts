fe8bfde151bcd9d31822568c5ca25f1b089bacb6
Rosana Moreira <rooh.moreiraa@gmail.com>
2021-07-29 15:51:22 -0300

    Aplicando os conceitos de DDD

--- server/src/modules/users/services/AuthenticateUserService.ts
+++ server/src/modules/users/services/AuthenticateUserService.ts
@@ -1,11 +1,10 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import authConfig from '../config/auth';
+import User from '@modules/users/infra/typeorm/entities/User';
+import authConfig from '../../../config/auth';
 
-import AppError from '../errors/AppError';
-
-import User from '../models/User';
+import AppError from '../../../shared/errors/AppError';
 
 interface Request {
   email: string;
--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -3,11 +3,11 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import routes from '@shared/infra/http/routes';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 app.use(cors());
