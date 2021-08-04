bcbe16128d6b9fae703d007876ee4fa313556c1a
Rosana Moreira <rooh.moreiraa@gmail.com>
2021-08-04 15:07:40 -0300

    injeçao de dependencias

---
+++ server/src/shared/container/index.ts
@@ -0,0 +1,16 @@
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepositiry';
+import { container } from 'tsyringe';
+
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentsRepository',
+  AppointmentsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
